52ad464fd570616b569a3a6284f971727f030d41
Jürgen Röhr <juergen@rapid-data.de>
2022-11-16 18:27:47 +0100

    Update prod settings to use eiche azure function

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  dataServerBaseAddress: 'https://eiche-infosystem-data.azurewebsites.net',
-  authCode: '89TXFKZJCU7Qm2HWjsgP7wuKtp4T3jBp_x-Bi4bjQSqsAzFucMS3-A=='
+  dataServerBaseAddress: 'https://grabbuch-daten.azurewebsites.net',
+  authCode: 'oB38RJRH6ha7PM69iBc4r4y9D3tNrZho5q-xx6k3Qar-AzFuTpBqkw=='
 };
